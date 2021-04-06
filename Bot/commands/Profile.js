4f90bedb6a3a5394a3565abeecffca87e5062500
Jaydecoder <78165724+Jaydecoder@users.noreply.github.com>
2021-04-06 11:09:17 +0100

    Update Profile.js

--- Bot/commands/Profile.js
+++ Bot/commands/Profile.js
@@ -1,9 +1,8 @@
 
 module.exports = {
-    name: 'profile',
-	description: 'Wanna know who i am? Well this will tell you!',
+    name: 'name of command',
+	description: 'description',
 	 execute(message, args) {
-		
-    message.channel.send("Hey, my name is Jay but my discord is Boris Johnson! I can actually program unlike most people, try not to boost my ego as its already pretty high.. Also DM me about anything")
+
 }
 }
