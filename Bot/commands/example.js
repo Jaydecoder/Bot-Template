333100ad22247d3c60676afe0d5190a3d07da0d5
Jaydecoder <78165724+Jaydecoder@users.noreply.github.com>
2021-05-16 19:03:31 +0100

    Update example.js

--- Bot/commands/example.js
+++ Bot/commands/example.js
@@ -1,3 +1,10 @@
+const embed = require("../../Utilities/embed").embed
+const getUser = require('../../firebase/firebaseconnect').getUserInfo
+const getCollection = require('../../firebase/firebaseconnect').getCollectionInfo
+const set = require('../../firebase/firebaseconnect').setUserInfo
+const Utilities = require("../../Utilities/Utlitlies")
+const config = require("../botconfig.json")
+const keygen = require("keygenerator")
 
 module.exports = {
     name: 'name of command',
