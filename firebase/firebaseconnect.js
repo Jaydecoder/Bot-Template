2325071689805ec51add584f3249c4c39eab9b8f
Jaydecoder <78165724+Jaydecoder@users.noreply.github.com>
2021-05-16 18:59:43 +0100

    Update firebaseconnect.js

--- firebase/firebaseconnect.js
+++ firebase/firebaseconnect.js
@@ -1,24 +1,58 @@
-const firebase = require("firebase")
+const admin = require('firebase-admin')
+const serviceAccount = require('./firebaseconfig.json')
+var keygen = require("keygenerator");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
 
-const firebaseConfig = require('./firebaseconfig.json')
-var initializefirebase = firebase.initializeApp(firebaseConfig);
-
-database = firebase.database();
+const db = admin.firestore()
 
 module.exports = {
-
-
-savedata: function(ref,data){
-
-    database.ref(ref).set(data, function(error) {if (error) {console.log("Failed with error: " + error)}})
-
-},
-
- getdata:function(ref){
-
+    getUserInfo: async(collection,id) => {
+        
     
-      return database.ref(ref).once('value').then(function(snapshot) { Data = snapshot.val();; return Data})
-
+        return db.collection(collection).doc(id)
+        .get().then(doc => {
+            if(doc.exists){
+                
+                return doc.data();
+            } else return;
+        
+        })
+    },
+    setUserInfo: async function(collection,Data) {
+        
+       if(!Data["DataId"]){
+            id =keygen._()
+            Data["DataId"] = id
+       }
+        
+    
+        return db.collection(collection).doc(Data["DataId"])
+        .set(Data).then(doc => 
+        {
             
-}
+            console.log("written new info to the database!")
+        })
+    },
+    getCollectionInfo: async (collection) => {
+        const snapshot = await db.collection(collection).get()
+        return snapshot.docs.map(doc => doc.data());
+    },
+    UpdateUserInfo: async function(collection,Data) {
+        return db.collection(collection).doc(Data["DataId"])
+        .update(Data).then(doc => 
+        {
+            
+            console.log("Updated new info to the database!")
+        })
+    },
+    DeleteUserInfo: async function(collection, Data){
+        return db.collection(collection).doc(Data["DataId"])
+        .delete().then(doc => 
+        {
+            
+            console.log("Deleted new info in the database!")
+        })
+    }
 }
